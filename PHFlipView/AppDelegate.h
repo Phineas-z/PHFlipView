2dc16987ac0cd1a71c5651682108c9c3d0339c52
Phineas <mcgradylue@gmail.com>
2013-05-15 13:16:58 +0800

    Initial Commit

---
+++ PHFlipView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PHFlipView
+//
+//  Created by luyuanshuo on 13-5-15.
+//  Copyright (c) 2013年 luyuanshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PHFlipView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PHFlipView
+//
+//  Created by luyuanshuo on 13-5-15.
+//  Copyright (c) 2013年 luyuanshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
